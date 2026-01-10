cbf9aba5ed066d607c871f0a968f8b9bffb8dcaf
brolfson <bennett.rolfson@ucas-edu.net>
2026-01-09 21:03:54 -0700

    Enhance savings function for better user input
    Refactor savings function to improve input handling and calculations.

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -33,27 +33,63 @@ def menu(options):
 #Create a function for Savings Time Calculator
 def savings():
     while True:
+        amount = input("How much money do you want to save up? ").strip()
+        if amount.isdigit():
+            amount = int(amount)
+            break
+        print("You did not input a number. Please try again. ")
+    while True:
         print("Your options are: \n1. daily \n2. weekly \n3. monthly \n4. annually")
-        check = input()
-        if check == "1":
+        time = input().strip()
+        if time == "1":
             time = "days"
             break
-        if check == "2":
+        if time == "2":
             time = "weeks"
             break
-        if check == "3":
+        if time == "3":
             time = "months"
             break
-        if check == "4":
+        if time == "4":
             time = "years"
             break
         else:
             print("You inputed an incorect input. Please input the number of the option that you want. ")
     while True:
-        check = input()
+        deposit = input("How much are you inputing each time? ").strip()
+        if check.isdigit():
+            deposit = int(deposit)
+            break
+        print("You did not input a number. Please try again. ")
+    print(f"It will take {amount/deposit} {time} to save up to ${amount}. ")
+    
 #Create a function for Compound Interest Calculator
+def compound():
+    while True:
+        start = input("What will be your starting amount? ").strip()
+        if start.isdigit():
+            start = int(start)
+            break
+        print("You did not input a number. Please try again. ")
+    while True:
+        rate = input("What will the interest rate be? ").strip()
+        if rate.isdigit():
+            rate = 1 + ((int(rate))/100)
+            break
+        print("You did not input a number. Please try again. ")
+    while True:
+        year = input("How many years will you leave the money alone? ").strip()
+        if year.isdigit():
+            year = int(year)
+            break
+        print("You did not input a number. Please try again. ")
+    final = start
+    for i in range(year):
+        final = rate * final
+    print(f"At the end of {year} years you will have ${final}. ")
 
 #Create a function for Budget Allocator
+def 
 
 #Create a function for Sale Price Calculator
 
