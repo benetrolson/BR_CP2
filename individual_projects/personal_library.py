a065b9789dde13a8e43bec33b6e6f2ba7ffa62a9
brolfson <bennett.rolfson@ucas-edu.net>
2026-01-17 13:51:24 -0700

    Add user options to personal library main function

--- individual_projects/personal_library.py
+++ individual_projects/personal_library.py
@@ -4,6 +4,9 @@
 def main():
   print("Welcome to your personal library. \nYour options are: \n1. View \n2. Add \n3. Remove \n4 Search \n5. Exit")
   check = input("What do you want to do? ")
+  if check == "1":
+    view()
+  elif check== "2":
 
 
 
