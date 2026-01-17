07e834b677298b582bc05e51d8c792435239a49c
brolfson <bennett.rolfson@ucas-edu.net>
2026-01-17 12:42:57 -0700

    Add initial structure for personal library program

--- individual_projects/personal_library.py
+++ individual_projects/personal_library.py
@@ -1,1 +1,17 @@
-#
+#BHR 2nd Personal Library
+
+
+def main():
+  print("Welcome to your personal library. \nYour options are: \n1. View \n2. Add \n3. Remove \n4 Search \n5. Exit")
+  check = input("What do you want to do? ")
+
+
+
+
+
+
+
+
+
+
+
