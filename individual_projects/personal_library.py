ea9c9c8555e2d60de8c4355cee976f4353f33834
brolfson <bennett.rolfson@ucas-edu.net>
2026-01-17 17:14:58 -0700

    Refactor main function to use library parameter
    Refactor main function to accept library as an argument and update main loop accordingly.

--- individual_projects/personal_library.py
+++ individual_projects/personal_library.py
@@ -1,20 +1,38 @@
 #BHR 2nd Personal Library
+library = []
 
-
-def main():
+def main(library):
   print("Welcome to your personal library. \nYour options are: \n1. View \n2. Add \n3. Remove \n4 Search \n5. Exit")
   check = input("What do you want to do? ")
   if check == "1":
     view()
   elif check== "2":
+    add()
+  elif check == "3":
+    remove()
+  elif check == "4":
+    search()
+  elif check == "5":
+    return "quit"
+  else:
+    print("That was not an option. Please try again. ")
+  return "", library
+
+#View function
+def view(library):
+  for i in library:
+    print(i)
+
+#Add function
 
 
+#Remove function
 
 
+#Search function
 
 
-
-
-
-
-
+#Main loop
+while True:
+  check, library = main(library)
+  if check == "quit"
