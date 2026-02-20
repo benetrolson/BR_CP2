0132bbed6e871d2561a1413a46e31e9d6ea5f346
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-20 07:57:42 -0700

    Import helper module in main.py

--- individual_projects/word_counter/src/main.py
+++ individual_projects/word_counter/src/main.py
@@ -1,9 +1,10 @@
 from time_finder import *
 from word_counter import *
+from helper import 
 
 def main():
     while True:
-        check = 
+        check = choice_input(["1", "2", "3", "4"], "Do you want to: \n1. Find the word count \n2. View the document \n3. ")
 
 
 
