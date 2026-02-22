9e900bb85172aa030ec60fc1a02849c9bb9b1a94
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-21 22:33:53 -0700

    Normalize input and add txt_reader function
    Updated input handling to normalize user input and added a new function for reading text files.

--- individual_projects/word_counter/src/helper.py
+++ individual_projects/word_counter/src/helper.py
@@ -2,8 +2,15 @@ import random as rand
 
 def choice_input(choices, prompt = ">"):
     while True:
-        choice = input(prompt)
+        choice = input(prompt).lower().strip()
         if choice in choices:
             return choice
+        elif choice in ["idk", "i don't know", "i dont know"]:
+            return rand.choice(choices)
         else:
             print("That was an invalid input. Please try again. ")
+
+def txt_reader(path):
+    try:
+        with open(path) as file:
+            
