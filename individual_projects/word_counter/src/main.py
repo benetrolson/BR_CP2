123d6ab553743b6a036a4019147ef105305cf0cd
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-21 18:22:17 -0700

    Add edit and exit options to main menu

--- individual_projects/word_counter/src/main.py
+++ individual_projects/word_counter/src/main.py
@@ -4,7 +4,7 @@ from helper import
 
 def main():
     while True:
-        check = choice_input(["1", "2", "3", "4"], "Do you want to: \n1. Find the word count \n2. View the document \n3. ")
+        check = choice_input(["1", "2", "3", "4"], "Do you want to: \n1. Find the word count \n2. View the document \n3. Edit the document \n4. Exit \n> ")
 
 
 
