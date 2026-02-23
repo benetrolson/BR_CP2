30ff8db95ee10833e677e1a90754e5c6017ee88b
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-22 19:01:09 -0700

    Update document_checker to read file with txt_reader
    Refactor file reading logic to use txt_reader function.

--- individual_projects/word_counter/src/document_checker.py
+++ individual_projects/word_counter/src/document_checker.py
@@ -1,9 +1,6 @@
 # BHR 2nd word counter
 
 def view():
-  try:
-    with open("docs\\document.txt", "r") as file:
-      
-    except FileNotFound:
-      print("The file was not found. ")
-      
+  file = txt_reader("docs\\document.txt")
+  print(file)
+  
