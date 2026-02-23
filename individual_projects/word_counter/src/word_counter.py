81e6c0800f7e5f45b02145a4f177f06aa3897278
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-22 21:34:46 -0700

    Simplify word_counter function
    Removed unnecessary variable initialization and checks.

--- individual_projects/word_counter/src/word_counter.py
+++ individual_projects/word_counter/src/word_counter.py
@@ -2,9 +2,6 @@
 from helper import *
 
 def word_counter():
-  word_count = 0
   document = txt_reader("docs\\document.txt")
-  if document is None:
-    return 0
   word_count = document.split()
   return len(word_count)
