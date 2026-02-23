00332f4d305c6c516e1f70b1c6f4b49604f20786
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-22 21:15:47 -0700

    Implement edit function for document modification
    Added an edit function to allow users to modify the document.

--- individual_projects/word_counter/src/document_checker.py
+++ individual_projects/word_counter/src/document_checker.py
@@ -4,3 +4,14 @@ def view():
   file = txt_reader("docs\\document.txt")
   print(file)
   
+def edit():
+  file = txt_reader("docs\\document.txt")
+  print("Press enter twice to finish")
+  view()
+  while True:
+    addition = input()
+    if not addition:
+      break
+    file.append(addition)
+  txt_saver("docs\\document.txt", file)
+    
