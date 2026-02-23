a47856c2e17682b727729a8659a7d3ebf72d3f4c
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-22 18:53:25 -0700

    Add word counting functionality to word_counter
    Initialize word count and update counting logic.

--- individual_projects/word_counter/src/word_counter.py
+++ individual_projects/word_counter/src/word_counter.py
@@ -1,6 +1,9 @@
 # BHR 2nd word counter
 
 def word_counter():
+  word_count = 0
   document = txt_reader("docs\\document.txt")
   for " " in document:
+    word_count += 1
+  return word_count
     
