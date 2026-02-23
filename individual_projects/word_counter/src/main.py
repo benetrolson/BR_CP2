7c43888ad0e974268d28faf751cd458d6a0d6cde
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-22 21:21:37 -0700

    Refactor if statements to elif for clarity

--- individual_projects/word_counter/src/main.py
+++ individual_projects/word_counter/src/main.py
@@ -8,11 +8,11 @@ def main():
         check = choice_input(["1", "2", "3", "4"], "Do you want to: \n1. Find the word count \n2. View the document \n3. Edit the document \n4. Exit \n> ")
         if check == "1":
             word_counter()
-        if check == "2":
+        elif check == "2":
             view()
-        if check == "3":
+        elif check == "3":
             edit()
-        if check == "4":
+        elif check == "4":
             break
 
 main()
