11ac8ee62d124a1ca3258773b6957796b994c875
benetrolson <bennett.rolfson@ucas-edu.net>
2026-02-27 10:51:36 -0700

    Perfected the fractal generator, just need to get bonus points.

--- individual_projects/fractal_generator.py
+++ individual_projects/fractal_generator.py
@@ -6,10 +6,13 @@ import turtle as t
 def main():
     while True:
         t.clearscreen()
-        quantity = int_input([1, 2, 3, 4, 5], "What do you want the recursion depth to be? (1-5). ")
-        color = choice_input(["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray", "white" ], "What do you want the color of the triangles to be? ")
+        quantity = int_input([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "What do you want the recursion depth to be? (1-5). ")
+        color = choice_input(["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray", "white", "bisque"], "What do you want the color of the triangles to be? ")
+        #t.tracer(0, 0)
         t.color(color)
+        t.teleport((-250/2**((quantity-5)*-1)), (-250/2**((quantity-5)*-1)))
         generator(quantity)
+        #t.update()
         check = input("Do you want to leave? ")
         if check == "yes":
             break
@@ -17,11 +20,16 @@ def main():
 # create the recursive function
 def generator(quantity):
     if quantity == 1:
+        for i in range(3):
+            t.forward(500 / (2**((quantity-5)*-1)))
+            t.left(120)
         return 1
     for i in range(3):
-        for i in range(2):
-            t.forward(500/(2 ** (quantity - 1)))
-            if i == 0: generator(quantity-1)
+        for i in range(3):
+            t.forward(500 / (2**((quantity-5)*-1)))
+            t.left(120)
+        generator(quantity -1)
+        t.forward(500 / (2**((quantity-5)*-1)))
         t.left(120)
 
 main()
