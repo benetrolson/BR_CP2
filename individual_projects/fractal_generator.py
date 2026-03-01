606037f613949f056a602d2f850adc434a207ce2
brolfson <bennett.rolfson@ucas-edu.net>
2026-02-28 21:40:51 -0700

    Refactor fractal generation and add koch function

--- individual_projects/fractal_generator.py
+++ individual_projects/fractal_generator.py
@@ -6,30 +6,48 @@ import turtle as t
 def main():
     while True:
         t.clearscreen()
-        quantity = int_input([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "What do you want the recursion depth to be? (1-5). ")
+        quantity = int_input([1, 2, 3, 4, 5], "What do you want the recursion depth to be? (1-5). ")
         color = choice_input(["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray", "white", "bisque"], "What do you want the color of the triangles to be? ")
-        #t.tracer(0, 0)
+        background_color = choice_input(["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray", "white", "bisque"], "What do you want the color of the background to be? ")
+        fractal = choice_input(["sierpinski", "sierpinski triangle", "koch", "koch snowflake"], "What fractal do you want to make? ")
+        t.tracer(0, 0)
         t.color(color)
+        t.bgcolor(background_color)
         t.teleport((-250/2**((quantity-5)*-1)), (-250/2**((quantity-5)*-1)))
-        generator(quantity)
-        #t.update()
+        if fractal in["sierpinski", "sierpinski triangle"]:
+            sierpinski(quantity)
+        elif fractal in ["koch", "koch snowflake"]:
+            koch(400, quantity)
+        t.update()
         check = input("Do you want to leave? ")
         if check == "yes":
             break
 
 # create the recursive function
-def generator(quantity):
+def sierpinski(quantity):
     if quantity == 1:
         for i in range(3):
             t.forward(500 / (2**((quantity-5)*-1)))
             t.left(120)
         return 1
     for i in range(3):
-        for i in range(3):
+        for _ in range(3):
             t.forward(500 / (2**((quantity-5)*-1)))
             t.left(120)
-        generator(quantity -1)
+        sierpinski(quantity -1)
         t.forward(500 / (2**((quantity-5)*-1)))
         t.left(120)
+
+def koch(length, depth):
+    if depth == 0:
+        t.forward(length)
+    else:
+        koch(length/3, depth-1)
+        t.left(60)
+        koch(length/3, depth-1)
+        t.right(120)
+        koch(length/3, depth-1)
+        t.left(60)
+        koch(length/3, depth-1)
 
 main()
